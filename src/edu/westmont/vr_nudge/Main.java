875fef1b6745e15ba2c9ad1769eda83b1431a93f
Donald J. Patterson <d_j_p_3@djp3.net>
2018-07-03 18:23:11 -0700

    One more tweak

--- src/edu/westmont/vr_nudge/Main.java
+++ src/edu/westmont/vr_nudge/Main.java
@@ -2,8 +2,26 @@ package edu.westmont.vr_nudge;
 
 public class Main {
 
+	private static final int DEGREE_STEPS = 10;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		VirtualMap vm = new VirtualMap("filename");
+		PhysicalMap pm = new PhysicalMap(400,400);
+		CostMap costs[][][] = new CostMap[vm.getLength()][vm.getWidth()][360/DEGREE_STEPS];
+		
+		
+		for(Integer w = 0; w < vm.getWidth();w++) {
+			for(Integer l = 0; l < vm.getLength();l++){
+				for(Integer degree = 0 ; degree < 360; degree += DEGREE_STEPS) {
+					CostMap cm = vm.calculateCostMap(pm,w,l,degree);
+				}
+				
+				
+			}
+		}
+		
+		
 
 	}
 
